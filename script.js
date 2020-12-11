a0d3ef69b233328e076aaa8491cdc91aa7237aab
Austin-Vincelli-Evans <unity7591@gmail.com>
2020-12-10 17:23:48 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,40 @@
+function playBells(){
+    const myBells = document.getElementById("bells")
+    myBells.play()
+}
+function playHo(){
+    const myHo = document.getElementById("hohoho")
+    myHo.play()
+}
+function playElf(){
+    const myElf = document.getElementById("elf")
+    myElf.play()
+}
+function playMagic(){
+    const myMagic = document.getElementById("magic")
+    myMagic.play()
+}
+function playDeck(){
+    const myDeck = document.getElementById("deck")
+    myDeck.play()
+}
+function playFall(){
+    const myFall = document.getElementById("fall")
+    myFall.play()
+}
+function playBuddy1(){
+    const myBuddy1 = document.getElementById("buddy1")
+    myBuddy1.play()
+}
+function playFragile(){
+    const myFragile = document.getElementById("fragile")
+    myFragile.play()
+}
+function playFilthyAnimal(){
+    const myAnimal = document.getElementById("filthyAnimal")
+    myAnimal.play()
+}
+function playBigTree(){
+    const myTree = document.getElementById("bigTree")
+    myTree.play()
+}
